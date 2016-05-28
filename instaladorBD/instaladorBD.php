b9c38add0d7f3feca1d7a1941d83520908de3f1a
Marcelo <ramirezmarcelo257@gmail.com>
2016-05-28 16:10:45 -0700

    validacion en javascript del registro de grupos

--- instaladorBD/instaladorBD.php
+++ instaladorBD/instaladorBD.php
@@ -13,33 +13,32 @@ function crearTablas(){
 	$cnn= new conexion();//crea instancia de la clase conexion
 	$con =$cnn->conectar();//la clase conexion almacenada de cnn ejecuta la funcion conectar.
 	mysqli_select_db($con,"sides");//mysqli_select_db(variableconexion,nombreBD)
-	
+
 	$sqlfalta="CREATE TABLE falta(
 	id_falta INT NOT NULL AUTO_INCREMENT,
 	nombre VARCHAR(150),
 	id_grupo INT,
 	PRIMARY KEY(id_falta)
-	)";//creamos la tabla
+	)";//creamos la tabla FALTA
 
 	$sqlgrupo="CREATE TABLE grupo(
 	id_grupo INT NOT NULL AUTO_INCREMENT,
 	grupo VARCHAR(60),
 	puntos INT,
 	PRIMARY KEY(id_grupo)
-	)";//creamos la tabla
-	
+	)";//creamos la tabla GRUPO.
+
 	if(mysqli_query($con,$sqlfalta)){
 		echo "tabla falta creada";
 	}
 	if(mysqli_query($con,$sqlgrupo)){
 		echo "tabla grupo creada";
 	}
-	
-
 	mysqli_close($con);
 }
 
 crearBD();
 crearTablas();
+
 
 ?>
